ea32496f600a901529d9aec19a90a0fb713109ed
Tif Taylor <tiftaylorisrad@gmail.com>
2020-08-31 15:19:54 -0700

    Added | Code to server and ejs files

---
+++ data/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO faves (title, artist, img, color1, color2, color3, color4)
+VALUES ('This thing', 'Charmander' 'https://i.imgur.com/J5LVHEL.jpg', '#58AADD', '#ADB1A1', '#2866E3', '#6B8922');
+
+
+INSERT INTO faves (title, artist, img, color1, color2, color3, color4)
+VALUES ('Another thing', 'Squirttle' 'https://i.imgur.com/J5LVHEL.jpg', '#58AAEE', '#ADB5A5', '#DF66E3', '#8A8922');
+
+
+
+
+
+
