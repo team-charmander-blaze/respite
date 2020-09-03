2abfb74c1f3cc3df9fcfacad614db134e451f457
Jack Nelson <jnelson.java@gmail.com>
2020-09-03 09:40:55 -0700

    added dynamic borders to hex codes

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,17 @@
 'use strict';
 
 $('#hexDiv').ready( () => {
-  const color1 = $('#hexDiv').children().eq(0).text();
   const color2 = $('#hexDiv').children().eq(1).text();
-  $('#hexDiv').css('background-color', color1);
   $('body').css('background-color', color2);
+
+  for (let i in $('.color-sample')) {
+    if ($('.color-sample').eq(i).text()) {
+      $('.color-sample').eq(i).css('border', `solid 3px ${$('.color-sample').eq(i).text()}`)
+    }
+  }
+})
+
+$('.color-sample').click((event) => {
+  console.log(event.target.innerText)
+  $('body').css('background-color', event.target.innerText);
 })
