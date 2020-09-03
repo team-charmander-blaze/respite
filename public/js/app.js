0d28352085ee6f061894dc4e21d037c8b90c06e6
Chandler Puckett <chandler.puckett1@gmail.com>
2020-09-03 15:42:58 -0700

    font styling

--- public/js/app.js
+++ public/js/app.js
@@ -6,12 +6,15 @@ $('#hexDiv').ready( () => {
 
   for (let i in $('.color-sample')) {
     if ($('.color-sample').eq(i).text()) {
-      $('.color-sample').eq(i).css('border', `solid 3px ${$('.color-sample').eq(i).text()}`)
+      $('.color-sample').eq(i).css('border', `solid 3px ${$('.color-sample').eq(i).text()}`);
+      $('.color-sample').eq(i).css('box-shadow', `#3037457a 2px 3px 4px 0px`);
+      $('.color-sample').eq(i).css('border-radius', `5px`);
     }
   }
-})
+});
 
 $('.color-sample').click((event) => {
 
   $('body').css('background-color', event.target.innerText).css('transition','background-color 2s ease');
-})
+});
+
