2ecd0fd3a36403847651b9704b53a8fb478f1442
Jack Nelson <jnelson.java@gmail.com>
2020-09-03 10:17:45 -0700

    removing console logs and commented out code

--- public/js/app.js
+++ public/js/app.js
@@ -12,6 +12,5 @@ $('#hexDiv').ready( () => {
 })
 
 $('.color-sample').click((event) => {
-  console.log(event.target.innerText)
   $('body').css('background-color', event.target.innerText);
 })
--- server.js
+++ server.js
@@ -41,25 +41,19 @@ function homePage (req, res) {
 
   superagent.get(apiQuery)
     .then(result => {
-      console.log(result.body);
       const quote = new Quotes(result);
-      console.log(quote);
       res.render('pages/index', {data: quote});
     })
     .catch(()=> {
       superagent.get(apiQuery)
         .then(result => {
-          console.log(result.body);
           const quote = new Quotes(result);
-          console.log(quote);
           res.render('pages/index', {data: quote});
         })
         .catch(()=>{
           superagent.get(apiQuery)
             .then(result => {
-              console.log(result.body);
               const quote = new Quotes(result);
-              console.log(quote);
               res.render('pages/index', {data: quote});
             });
         });
